78130ac8e3f3ec11c2c61ee554c1fe3f6db9ee32
CarlieMartece <carliemartece@gmail.com>
2023-06-21 15:29:48 +0100

    add prefetching and optimistic rendering

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,8 @@
-import type { ActionArgs, LinksFunction, V2_MetaFunction } from "@remix-run/node";
+import type {
+  ActionArgs,
+  LinksFunction,
+  V2_MetaFunction,
+} from "@remix-run/node";
 import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 
 import stylesUrl from "~/styles/login.css";
@@ -11,8 +15,7 @@ export const links: LinksFunction = () => [
 ];
 
 export const meta: V2_MetaFunction = () => {
-  const description =
-    "Login to submit your own jokes to Remix Jokes!";
+  const description = "Login to submit your own jokes to Remix Jokes!";
 
   return [
     { name: "description", content: description },
